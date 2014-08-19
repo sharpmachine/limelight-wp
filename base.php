8959a64c497e71967496d77a8f62769dc6145fb0
Jesse Kade <jessekade@gmail.com>
2014-08-19 15:35:23 -0700

    Tweaks to base.php

--- base.php
+++ base.php
@@ -13,8 +13,8 @@
   ?>
 
   <div class="wrap" role="document">
-    <div class="content row">
-      <main class="main <?php echo roots_main_class(); ?>" role="main">
+    <div class="content">
+      <main class="main" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
       <?php if (roots_display_sidebar()) : ?>
--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,10 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
+  <link rel="icon"
+        type="image/ico"
+        href="<?php bloginfo('template_directory'); ?>/assets/img/favicon.ico" />
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
